7ea253d90dda807f8988f1119eea12f794416469
Kerssandra <Kerssandra@users.noreply.github.com>
2019-02-04 11:18:27 -0400

    Added source file

---
+++ Strings Lab 1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+using namespace std;
+ int main()
+{
+	string s0;
+	string s1 = "hello";
+	string s2 = s1 + " world";
+	
+	cout << "s0 =" << s0 << endl;
+	cout << "s1 =" << s1 <<endl;
+	cout << "the 4th character of s1 is " << ws1[3] << endl;
+	cout << "s1 has" <<s1.size() << "characters" << endl;
+	
+	if (s1 = = "hello")
+		cout << "hi there" <<endl;
+	if (s2 ! = "world")
+		cout << "out of this world" << endl;
+}
+	
